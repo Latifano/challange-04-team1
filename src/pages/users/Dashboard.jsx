f7507f8413488b16f3599b39d31c6c325599e500
Latifano <ikromilatifano76@gmail.com>
2023-05-19 11:02:33 +0700

    add redux

--- src/pages/users/Dashboard.jsx
+++ src/pages/users/Dashboard.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+// import { useSelector, useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
+// import { getMe } from "../../redux/actions/authActions";
 
 function Dashboard() {
   const [user, setUser] = useState("");
+
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  // const { user } = useSelector((state) => state.auth);
 
   useEffect(() => {
     const getMe = async () => {
@@ -42,9 +49,15 @@ function Dashboard() {
     getMe();
   }, []);
 
+  // useEffect(() => {
+  //   dispatch(getMe(navigate, null, null));
+  // }, [dispatch, navigate]);
+
   return (
     <Container className="p-4">
-      <h1 className="text-center">Hi, {user?.name}!</h1>
+      <h1 className="text-center">
+        Hi, {user?.name} with {user?.email}!
+      </h1>
       <h1 className="text-center">
         This page only can be accessed by user having login
       </h1>
